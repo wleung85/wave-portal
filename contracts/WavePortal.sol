d77dcd8e88b1fb7879548945a46e433a4448b59a
Winston Leung <wleung85@uwo.ca>
2021-10-10 15:50:11 -0400

    Added functionality to store the number of waves from a user and simulated other users waving

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,26 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint256 totalWaves;
+  mapping (address => uint256) private addrWaveCount;
+
   constructor() {
     console.log("Smart contract successfully deployed!");
+  }
+
+  function wave() public {
+    totalWaves += 1;
+    addrWaveCount[msg.sender] += 1;
+    console.log("%s has waved!", msg.sender);
+  }
+
+  function getTotalWaves() public view returns (uint256) {
+    console.log("We have %d total waves!", totalWaves);
+    return totalWaves;
+  }
+
+  function getAddrWaveCount() public view returns (uint256) {
+    console.log("%s has waved %s times!", msg.sender, addrWaveCount[msg.sender]);
+    return addrWaveCount[msg.sender];
   }
 }
