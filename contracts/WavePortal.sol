addbd803f9c27c147a7ea03c6873313f648ffa49
Winston Leung <wleung85@uwo.ca>
2021-10-10 15:25:52 -0400

    Added deploying smart contract to local blockchain network

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,6 +5,6 @@ import "hardhat/console.sol";
 
 contract WavePortal {
   constructor() {
-    console.log("Yo yo, I am a contract and I am smart");
+    console.log("Smart contract successfully deployed!");
   }
 }
