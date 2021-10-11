98bbf71927700876178651bc21a04ef23792006c
Winston Leung <wleung85@uwo.ca>
2021-10-11 00:03:13 -0400

    Waving now gives Ether back to waver

--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,28 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
-  const waveContract = await waveContractFactory.deploy();
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther('0.1'),
+  });
   await waveContract.deployed();
 
   console.log("Contract deployed to:", waveContract.address);
   console.log("Contract deployed by:", owner.address);
 
+  /* Get contract balance */
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    'Contract balance: ', hre.ethers.utils.formatEther(contractBalance)
+  );
+
   /* We don't need to call waveCount.wait() because getTotalWaves() is a view,
-   * i.e. does not affect anything in the blockchain and is read-only */
+   * i.e. does not affect anything in the blockchain and is read-only.
+   * We use toNumber() because getTotalWaves returns a uint256 which is a
+   * BigNumber and needs to be converted from the object into a number. */
   let waveCount = await waveContract.getTotalWaves();
-  console.log(waveCount.toNumber());
+  console.log(waveCount.toNumber()); 
 
   /* Sending waves */
   let waveTxn = await waveContract.wave('A message!');
@@ -22,9 +34,17 @@ const main = async () => {
   waveCount = await waveContract.getTotalWaves();
   console.log("Total number of waves: %d", waveCount);
 
-  // Get how many times owner has waved
+  /* Get how many times owner has waved */
   let userWaveCount = await waveContract.getAddrWaveCount();
-  console.log("Total number of waves from %d: %d", owner, userWaveCount);
+  console.log("Total number of waves from %s: %d", owner.address, userWaveCount);
+
+  /* Get contract balance to see what happened */
+  contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    'Contract balance: ', hre.ethers.utils.formatEther(contractBalance)
+  );
 
   let allWaves = await waveContract.getAllWaves();
   console.log(allWaves);
